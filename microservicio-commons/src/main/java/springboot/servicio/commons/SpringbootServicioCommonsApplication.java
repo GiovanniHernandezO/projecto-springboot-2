ea6a87e718a7eafeaf47ac32d3eccc95df3dd36e
GiovanniHernandezO <giovanni.hernandez.olivares@gmail.com>
2023-11-01 18:59:46 -0300

    Proyecto COMMMONS inicial

---
+++ microservicio-commons/src/main/java/springboot/servicio/commons/SpringbootServicioCommonsApplication.java
@@ -0,0 +1,13 @@
+package springboot.servicio.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioCommonsApplication.class, args);
+	}
+
+}
