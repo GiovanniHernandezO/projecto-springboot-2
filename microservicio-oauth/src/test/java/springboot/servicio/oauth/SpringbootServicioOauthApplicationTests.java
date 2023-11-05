9188c6b460c85e3464674bfd13f96fec7702c6cf
GiovanniHernandezO <giovanni.hernandez.olivares@gmail.com>
2023-11-04 23:40:25 -0300

    Proyecto OAUTH configuracion inicial!

---
+++ microservicio-oauth/src/test/java/springboot/servicio/oauth/SpringbootServicioOauthApplicationTests.java
@@ -0,0 +1,8 @@
+package springboot.servicio.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioOauthApplicationTests {
+}
