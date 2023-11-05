6a8ca76f15776310763b6f93009b8013b1894b6d
GiovanniHernandezO <giovanni.hernandez.olivares@gmail.com>
2023-11-04 23:10:12 -0300

    Continuacion proyecto usuarios

---
+++ microservicio-usuarios/src/main/java/springboot/servicio/usuarios/config/RepositoryConfig.java
@@ -0,0 +1,20 @@
+package springboot.servicio.usuarios.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import springboot.servicio.usuarios.models.entity.Role;
+import springboot.servicio.usuarios.models.entity.Usuario;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        /*
+        GH: esta configuracion permite mostrar los identificadores de las entidades/tablas en el JSON
+        por defecto no muestra el ID pero gracias al exposeIdsFor se pueden mostrar
+        * */
+        config.exposeIdsFor(Usuario.class, Role.class);
+    }
+}
---
+++ microservicio-usuarios/src/main/java/springboot/servicio/usuarios/models/dao/IUsuarioDAO.java
@@ -0,0 +1,23 @@
+package springboot.servicio.usuarios.models.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import springboot.servicio.usuarios.models.entity.Usuario;
+
+@RepositoryRestResource(path = "usuarios")
+public interface IUsuarioDAO extends CrudRepository<Usuario, Long> {
+
+    @RestResource(path = "buscar-username")
+    Usuario findByUsername(@Param("nombre") String username);
+
+    @RestResource(path = "obtener-por-query")
+    @Query(value = "select u from Usuario u where u.username = ?1")
+    Usuario obtenerPorUsername(@Param("nombre-usuario") String username);
+
+    @RestResource(path = "obtener-por-query-nativa")
+    @Query(value = "select * from usuarios u where u.username = ?1", nativeQuery = true)
+    Usuario obtenerPorUsernameFormaNativaSQL(@Param("nombre-usuario-nativo")String username);
+}
