45170793f9e26a6ea48564dce0b0e781bfd0c36d
GiovanniHernandezO <giovanni.hernandez.olivares@gmail.com>
2023-11-07 23:31:14 -0300

    Se agrega configuracion a API GATEWAY de JWT, de manera reactiva - SIN PRUEBAS - FALLANDO falta revisar problema de 401

---
+++ microservicio-gateway-server/src/main/java/springboot/servicio/gateway/server/security/SpringSecurityConfig.java
@@ -0,0 +1,39 @@
+package springboot.servicio.gateway.server.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter authenticationFilter;
+
+    @Bean
+    public SecurityWebFilterChain configure(ServerHttpSecurity httpSecurity) {
+
+        return httpSecurity
+                .authorizeExchange()
+                .pathMatchers("/api-security/oauth/**").permitAll()
+                .pathMatchers(HttpMethod.GET,
+                        "/api-productos/productos/listar",
+                        "/api-items/items/listar",
+                        "/api-usuarios/usuarios",
+                        "/api-items/items/produtos/{id}/cantidad/{id}",
+                        "/api-productos/{id}").permitAll()
+                .pathMatchers(HttpMethod.GET,
+                        "/api-usuarios/usuarios/{id}").hasAnyRole("ADMIN", "USER")
+                .pathMatchers("/api-productos/**", "/api-items/**", "/api-usuarios/**").hasRole("ADMIN")
+                .anyExchange()
+                .authenticated()
+                .and()
+                .addFilterAt(authenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .csrf().disable()
+                .build();
+    }
+}
